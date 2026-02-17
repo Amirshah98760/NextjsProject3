9d5ce5f410b4765f505f6443e5f2970c6efe5b8f
Amirshah98760 <amirshah98760@gmail.com>
2026-02-17 15:28:10 +0500

    Design the login and register

--- app/(Auth)/login/page.jsx
+++ app/(Auth)/login/page.jsx
@@ -4,13 +4,32 @@ import Link from 'next/link'
 const page = () => {
   return (
     <div className="flex items-center justify-center h-screen flex-col gap-2 border border-amber-300" >
-      <h1 className="text-3xl font-bold">Login Page</h1>
-      <p className="description">
-        This is the login page of the Next.js application.
-      </p>
-       <h2>
+     <form >
+
+        <h1 className="text-3xl font-bold mb-3 text-center">Login Page</h1>
+        <p className="mb-4 text-center">
+          This is the login page of the Next.js application.
+        </p>
+        <input
+          type="text"
+          placeholder="Username"
+            className="border border-gray-300 rounded px-4 py-3 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-amber-500"
+        />
+        <input
+          type="password"   
+            placeholder="Enter your Password"
+            className="border border-gray-300 rounded px-4 py-3 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-amber-500"
+        />
+        <button
+          type="submit"
+            className="bg-amber-500 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded w-full cursor-pointer"
+        >
+          Login
+        </button>
+     </form>
+     <h2>
    If you already have an account, {" "}
-      <Link href="/register" className='text-blue-400'>
+      <Link href="/register" className='text-blue-700'>
      Register
       </Link>
       </h2>
--- app/(Auth)/register/page.jsx
+++ app/(Auth)/register/page.jsx
@@ -4,13 +4,37 @@ import Link from 'next/link'
 const page = () => {
   return (
     <div className="flex items-center justify-center h-screen flex-col gap-2 border border-amber-300" >
-      <h1 className="text-3xl font-bold">Register Page</h1>
-      <p className="description">
-        This is the register page of the Next.js application.
-      </p>
+     <form className='max-w-2xl'>
+
+        <h1 className="text-3xl font-bold mb-2 text-center">Register Page</h1>
+        <p className="mb-4 text-center">
+          This is the register page of the Next.js application.
+        </p>
+        <input
+          type="text"
+          placeholder="Username"
+        className="border border-gray-300 rounded px-4 py-3 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-amber-500"
+        />
+        <input
+          type="text"
+          placeholder="Email"
+            className="border border-gray-300 rounded px-4 py-3 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-amber-500"
+        />
+        <input
+          type="password"   
+            placeholder="Enter your Password"
+            className="border border-gray-300 rounded px-4 py-3 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-amber-500"
+        />
+        <button
+          type="submit"
+            className="bg-amber-500 hover:bg-amber-600 text-white font-bold py-2 px-4 rounded w-full focus:outline-none focus:ring-2 focus:ring-amber-500 cursor-pointer"
+        >
+          Register
+        </button>
+     </form>
       <h2>
    If you already have an account, {" "}
-      <Link href="/login" className='text-blue-400'>
+      <Link href="/login" className='text-blue-700'>
      Login
       </Link>
       </h2>
